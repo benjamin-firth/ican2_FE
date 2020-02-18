6a7128a651d999fbf62ad04c713a2b9e4b7a7632
Ben Firth <bfirth93@gmail.com>
2020-02-18 14:42:03 -0700

    Create user profile template

---
+++ src/components/UserProfile/UserProfile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const UserProfile = () => {
+
+  return (
+    <section>
+      <h2>Name</h2>
+      <p>Description:</p>
+      <p>Lorom ipsum words yay girl what do I even say here? I'm not great at coming up with these things. This is what I should be practicing instead of coding...</p>
+    </section>
+  )
+}
+
+export default UserProfile;
