f2efc7899de2908ba37cffa2a2530b3bd2c9a237
Ben Firth <bfirth93@gmail.com>
2020-02-18 19:07:03 -0700

    Add nav button snapshots

---
+++ src/components/NavButton/NavButton.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NavButton from './NavButton';
+
+describe('NavButton', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow( <NavButton />)
+  });
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
