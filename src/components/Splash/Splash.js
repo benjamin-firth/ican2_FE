4a45ed251e6d90a36bc643193fae2e747d6074f9
Kayla Wood <kaylaewood@gmail.com>
2020-02-19 13:50:19 -0700

    Add welcome message to splash page

--- src/components/Splash/Splash.js
+++ src/components/Splash/Splash.js
@@ -6,6 +6,7 @@ const Splash = () => {
 
   return (
     <section className='splash-container'>
+      <p>welcome! <br /> we are so glad you're here.</p>
       <button className='login-button'>log in</button>
       <button className='sign-up-button'>sign up</button>
       <Link to='/about'>learn more</Link>
