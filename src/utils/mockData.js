729613d5993470d952ccfabd01f3bd9b7efc7a80
Ben Firth <bfirth93@gmail.com>
2020-02-18 17:31:53 -0700

    Add mock data

---
+++ src/utils/mockData.js
@@ -0,0 +1,90 @@
+const mockMentors = [
+  {
+    id: 1,
+    name: 'Kayla',
+    email: 'kayla@email.com',
+    password: 'password',
+    location: '80401',
+    currentField: 'Software',
+    experienceLevel: 'Expert',
+    mentee: false,
+    mentor: true,
+    aboutMe: 'Lorum ipsum blah blah blah blah blah blah blah boah what is going on eh?',
+    picture: 'profilePicture.jpg',
+    workDay: 'Lorum ipsum blah blah blah.',
+    enjoyment: 'Blah blah blah boah what is going on.',
+    teachingPoints: 'Teachin test mock stuff.',
+    advice: 'mock advice is really nice!'
+  },
+  {
+    id: 2,
+    name: 'John Adams',
+    email: 'John@email.com',
+    password: 'password',
+    location: '80401',
+    currentField: 'Culinary',
+    experienceLevel: 'Expert',
+    mentee: false,
+    mentor: true,
+    aboutMe: 'Lorum ipsum blah blah blah blah blah blah blah boah what is going on eh?',
+    picture: 'profilePicture.jpg',
+    workDay: 'Lorum ipsum blah blah blah.',
+    enjoyment: 'Blah blah blah boah what is going on.',
+    teachingPoints: 'Teachin test mock stuff.',
+    advice: 'mock advice is really nice!'
+  },
+  {
+    id: 3,
+    name: 'Whitney',
+    email: 'Whitney@email.com',
+    password: 'password',
+    location: '80401',
+    currentField: 'Software',
+    experienceLevel: 'Expert',
+    mentee: false,
+    mentor: true,
+    aboutMe: 'Lorum ipsum blah blah blah blah blah blah blah boah what is going on eh?',
+    picture: 'profilePicture.jpg',
+    workDay: 'Lorum ipsum blah blah blah.',
+    enjoyment: 'Blah blah blah boah what is going on.',
+    teachingPoints: 'Teachin test mock stuff.',
+    advice: 'mock advice is really nice!'
+  }
+];
+
+const mockMentees = [
+  {
+    id: 10,
+    name: 'Ben',
+    email: 'ben@email.com',
+    password: 'password',
+    location: '80401',
+    currentField: 'Software',
+    experienceLevel: 'Beginner',
+    mentee: true,
+    mentor: false,
+    aboutMe: 'Lorum ipsum blah blah blah blah blah blah blah boah what is going on eh?',
+    picture: 'profilePicture.jpg',
+    workDay: 'Lorum ipsum blah blah blah.',
+    enjoyment: 'Blah blah blah boah what is going on.',
+    teachingPoints: 'Teachin test mock stuff.',
+    advice: 'mock advice is really nice!'
+  },
+  {
+    id: 11,
+    name: 'Noah',
+    email: 'noah@email.com',
+    password: 'password',
+    location: '80401',
+    currentField: 'Culinary',
+    experienceLevel: 'Beginner',
+    mentee: true,
+    mentor: false,
+    aboutMe: 'Lorum ipsum blah blah blah blah blah blah blah boah what is going on eh?',
+    picture: 'profilePicture.jpg',
+    workDay: 'Lorum ipsum blah blah blah.',
+    enjoyment: 'Blah blah blah boah what is going on.',
+    teachingPoints: 'Teachin test mock stuff.',
+    advice: 'mock advice is really nice!'
+  }
+];
