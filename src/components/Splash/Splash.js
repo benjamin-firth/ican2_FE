0e457308da25319c221a2da1bfef88e29223e713
Kayla Wood <kaylaewood@gmail.com>
2020-02-19 13:52:40 -0700

    Adjust spacing on splash page

--- src/components/Splash/Splash.js
+++ src/components/Splash/Splash.js
@@ -9,7 +9,7 @@ const Splash = () => {
       <p>welcome! <br /> we are so glad you're here.</p>
       <button className='login-button'>log in</button>
       <button className='sign-up-button'>sign up</button>
-      <Link to='/about'>learn more</Link>
+      <Link to='/about'>learn more about ican2</Link>
     </section>
   );
 }
