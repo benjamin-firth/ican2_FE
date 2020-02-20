f4b7a54754d846564471f334a5cead7a240f646a
Ben Firth <bfirth93@gmail.com>
2020-02-19 19:36:31 -0700

    Add local storage to signup form

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './SignUpForm.scss';
 
 const SignUpForm = () => {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [location, setLocation] = useState('');
+  const [field, setField] = useState('');
+  const [expertise, setExpertise] = useState('');
+
 
   return (
     <section>
       <h3>build your profile</h3>
       <form>
         <label>WHAT IS YOUR FIRST NAME?</label>
-        <input />
+        <input onChange={(e) => setName(e.target.value)}/>
         <label>WHAT IS YOUR EMAIL ADDRESS?</label>
-        <input />
+        <input onChange={(e) => setEmail(e.target.value)}/>
         <label>WHAT IS YOUR CURRENT CITY AND STATE?</label>
-        <input />
+        <input onChange={(e) => setLocation(e.target.value)}/>
         <label>WHAT FIELD ARE YOU CURRENTLY WORKING IN?</label>
-        <input />
+        <input onChange={(e) => setField(e.target.value)}/>
         <label>WHAT IS YOUR LEVEL OF EXPERTISE IN YOUR CURRENT FIELD?</label>
-        <input />
+        <select onChange={(e) => setExpertise(e.target.value)}>
+          <option>Beginner</option>
+          <option>Intermediate</option>
+          <option>Adanced</option>
+        </select>
         <button>SUBMIT</button>
       </form>
     </section>
