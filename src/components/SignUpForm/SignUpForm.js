be46c1a46364bbf348089f359d5ae92220d10541
Ben Firth <bfirth93@gmail.com>
2020-02-19 19:00:54 -0700

    Add signup form jsx

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -4,9 +4,22 @@ import './SignUpForm.scss';
 const SignUpForm = () => {
 
   return (
-    <form>
-      <p>This the sign up form.</p>
-    </form>
+    <section>
+      <h3>build your profile</h3>
+      <form>
+        <label>WHAT IS YOUR FIRST NAME?</label>
+        <input />
+        <label>WHAT IS YOUR EMAIL ADDRESS?</label>
+        <input />
+        <label>WHAT IS YOUR CURRENT CITY AND STATE?</label>
+        <input />
+        <label>WHAT FIELD ARE YOU CURRENTLY WORKING IN?</label>
+        <input />
+        <label>WHAT IS YOUR LEVEL OF EXPERTISE IN YOUR CURRENT FIELD?</label>
+        <input />
+        <button>SUBMIT</button>
+      </form>
+    </section>
   );
 }
 
