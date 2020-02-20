127cccbc5bd4a4e26358c8f34d31ee471f14b519
Kayla Wood <kaylaewood@gmail.com>
2020-02-19 21:22:11 -0700

    Add basic form validation to login

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginForm.scss';
 
 const LoginForm = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const enterEmail = (e) => {
+    setError('');
+    setEmail(e.target.value);
+  }
+
+  const enterPassword= (e) => {
+    setError('');
+    setPassword(e.target.value);
+  }
+
+  const login = (e) => {
+    e.preventDefault();
+    if (!email.length || !password.length) {
+      setError('Please be sure you have filled out all sections.')
+    } else {
+      console.log({ email: email, password: password });
+    }
+  }
 
   return (
     <section className='login-page'>
       <form className='login-form'>
         <div>
-          <p>USERNAME</p>
-          <input type='text' />
+          <p>EMAIL</p>
+          <input type='text' onChange={(e) => enterEmail(e)}/>
         </div>
         <div>
           <p>PASSWORD</p>
-          <input type='text' />
+          <input type='text' onChange={(e) => enterPassword(e)}/>
         </div>
-        <button className='login-submit-button'>enter</button>
+        {error && <p className="error-msg">{error}</p>}
+        <button className='login-submit-button' onClick={(e) => login(e)}>enter</button>
       </form>
     </section>
   );
