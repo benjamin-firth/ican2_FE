469af298e9898cf42aa0acad6dd51e1e6775ff82
Kayla Wood <kaylaewood@gmail.com>
2020-02-19 20:45:03 -0700

    Add routes to splash page buttons

--- src/components/Splash/Splash.js
+++ src/components/Splash/Splash.js
@@ -7,8 +7,12 @@ const Splash = () => {
   return (
     <section className='splash-container'>
       <p>welcome! <br /> we are so glad you're here.</p>
-      <button className='login-button'>log in</button>
-      <button className='sign-up-button'>sign up</button>
+      <Link to='/login'>
+        <button className='login-button'>log in</button>
+      </Link>
+      <Link to='/signup'>
+        <button className='sign-up-button'>sign up</button>
+      </Link>
       <Link to='/about'>learn more about ican2</Link>
     </section>
   );
