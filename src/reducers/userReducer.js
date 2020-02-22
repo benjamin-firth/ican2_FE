7cc58d9e560024f467a6900a7a57cdf1b0844e50
Ben Firth <bfirth93@gmail.com>
2020-02-22 13:23:46 -0700

    Add to user reducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -2,6 +2,8 @@ export const userReducer = (state = {}, action) => {
   switch (action.type) {
     case 'ADD_CURRENT_USER' :
       return action.user;
+    case 'SET_NEW_USER' :
+      return 
     default:
       return state;
   }
