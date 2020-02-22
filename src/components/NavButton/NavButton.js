349593ace03fdfe4f034f178714748d6916275a2
Kayla Wood <kaylaewood@gmail.com>
2020-02-22 11:00:09 -0700

    Add getUser method

--- src/components/NavButton/NavButton.js
+++ src/components/NavButton/NavButton.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import { useParams} from "react-router";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import './NavButton.scss';
 
 const NavButton = ({ nav, name }) => {
-  // let params = useParams();
-  // console.log(params);
-  // console.log(nav.split('/')[1]);
-  // console.log(window.location.pathname);
 
   const checkSelected = () => {
     if (window.location.pathname === nav) {
