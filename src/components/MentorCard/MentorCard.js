9b992fc3d5a31994354d558b7002cd2e33cb51bc
Kayla Wood <kaylaewood@gmail.com>
2020-02-22 15:02:49 -0700

    Add mentor cards

--- src/components/MentorCard/MentorCard.js
+++ src/components/MentorCard/MentorCard.js
@@ -4,9 +4,23 @@ import './MentorCard.scss';
 const MentorCard = ({ mentor }) => {
 
   return (
-    <section>
-      <p>Hi I am {mentor.name}</p>
-    </section>
+    <div className='background'>
+      <section>
+        <div className='image-box'>
+          <img
+            src={mentor.profile.image}
+            alt='mentor profile picture'
+            className='profile-photo'/>
+          <button>connect</button>
+        </div>
+        <div className='desc-box'>
+          <h2>{mentor.name.toLowerCase()}</h2>
+          <p>{mentor.location.city}, {mentor.location.state}</p>
+          <h3>{mentor.mentorProfile.fieldOfKnowledge}</h3>
+          <p>{mentor.profile.aboutMe}</p>
+        </div>
+      </section>
+    </div>
   );
 }
 
