6e0bbbd7a09862a9538695aed84bde3cd7399c92
Kayla Wood <kaylaewood@gmail.com>
2020-02-22 09:12:32 -0700

    Add selected class to NavButtons

--- src/components/NavButton/NavButton.js
+++ src/components/NavButton/NavButton.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useParams} from "react-router";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './NavButton.scss';
 
 const NavButton = ({ nav, name }) => {
+  // let params = useParams();
+  // console.log(params);
+  // console.log(nav.split('/')[1]);
+  // console.log(window.location.pathname);
+
+  const checkSelected = () => {
+    if (window.location.pathname === nav) {
+      return 'selected';
+    } else {
+      return ''
+    }
+  }
 
   return (
     <Link to={nav}>
-      <button>
+      <button className={checkSelected()}>
         {name}
       </button>
     </Link>
