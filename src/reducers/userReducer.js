381f09b674e97242094cd281e9c12def7502c4e2
Ben Firth <bfirth93@gmail.com>
2020-02-22 13:32:06 -0700

    Change userReducder to loginReducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,11 +1,9 @@
-export const userReducer = (state = {}, action) => {
+export const loginReducer = (state = {}, action) => {
   switch (action.type) {
     case 'LOG_IN' :
       return action.user;
     case 'LOG_OUT' :
       return {};
-    case 'SET_NEW_USER' :
-      return 
     default:
       return state;
   }
