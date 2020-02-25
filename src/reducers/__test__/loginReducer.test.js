e7fac230b4bf67cf77735aa9842584b887a54165
Ben Firth <bfirth93@gmail.com>
2020-02-25 12:02:29 -0700

    Add loginReducer tests

---
+++ src/reducers/__test__/loginReducer.test.js
@@ -0,0 +1,50 @@
+import { loginReducer } from '../loginReducer';
+
+describe('loginReducer', () => {
+  it('should return the initial state', () => {
+    const expected = {};
+
+    const result = loginReducer(undefined, {});
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return the correct state if the action is LOG_IN', () => {
+    const initialState = {};
+    const user = 'user';
+    const expected = 'user';
+
+    const action = {
+      type: 'LOG_IN',
+      user
+    };
+
+    const result = loginReducer(initialState, action);
+
+    expect(result).toEqual(expected);
+  });
+//
+  it('should return the correct state if the action is LOG_OUT', () => {
+    const initialState = {};
+    const user = 'user';
+    const expected = {};
+
+    const loginAction = {
+      type: 'LOG_IN',
+      user
+    };
+
+    const logoutAction = {
+      type: 'LOG_OUT',
+      user
+    };
+
+    loginReducer(initialState, loginAction);
+
+    const result = loginReducer(initialState, logoutAction);
+
+    expect(result).toEqual(expected);
+
+    expect(result).toEqual(expected);
+  });
+});
