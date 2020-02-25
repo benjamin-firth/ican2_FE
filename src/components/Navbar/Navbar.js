160b6aaf92be5e0f09ee734570f0bce2a5326cac
Kayla Wood <kaylaewood@gmail.com>
2020-02-25 10:48:40 -0700

    Remove unused buttons

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import NavButton from '../NavButton/NavButton';
 import simpleLogo from '../../images/simple-logo.png';
 
 const Navbar = () => {
-  const buttons = [{nav: '/mentors', name: 'find mentors'}, {nav: '/messages', name: 'messages'}, {nav: '/meetups', name: 'meetups'}, {nav: '/myprofile', name: 'my profile'}, {nav: '/', name: 'log out'}];
+  const buttons = [{nav: '/mentors', name: 'find mentors'}, {nav: '/messages', name: 'messages'}, {nav: '/myprofile', name: 'my profile'}, {nav: '/', name: 'log out'}];
 
   const renderButtons = () => {
     return buttons.map(button => (
