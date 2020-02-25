9d539c6b6ad50c9dfcf849b9bf852446851c6ab2
Ben Firth <bfirth93@gmail.com>
2020-02-25 12:36:05 -0700

    Add apiCalls testing

---
+++ src/utils/apiCalls.test.js
@@ -0,0 +1,24 @@
+import { fetchData } from './apiCalls';
+
+describe('api', () => {
+  describe('fetchData', () => {
+    const mockEmail = 'bf@email.com';
+    const mockBody = {"query": "{users(email: \""+ mockEmail + "\") {id name email mentor profile {gender aboutMe image fieldOfInterest} mentorProfile {fieldOfKnowledge experienceLevel workDayQuestion enjoymentQuestion teachingPointsQuestion adviceQuestion} location {city state}}}"};
+
+    const mockResponse = {id: 3, name: 'Ben', email: 'bf@email.com', mentor: '', profile: {gender: 'He/Him', aboutMe: 'Eh', image: 'image', fieldOfInterest: 'none'}, mentorProfile: {fieldOfKnowledge: 'none', experienceLevel: 'Advanced', workDayQuestion: 'none', enjoymentQuestion: 'none', teachingPointsQuestion: 'none', adviceQuestion: 'none'}, location: {city: 'Olympia', state: 'WA'}}
+
+    beforeEach(() => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockResponse)
+        });
+      });
+    });
+
+    it('should call fetch POST with the correct param', async () => {      
+      await fetchData(mockBody);
+      expect(window.fetch).toHaveBeenCalled();
+    });
+  });
+});
