b1f2933b89e489e39070f14f631c369aeb3ee3e6
Kayla Wood <kaylaewood@gmail.com>
2020-02-25 18:37:01 -0700

    Add styling to filter

--- src/components/FilterBox/FilterBox.js
+++ src/components/FilterBox/FilterBox.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FilterBox.scss';
 
 const FilterBox = () => {
+  const [fieldFilter, setFieldFilter] = useState('');
   window.scrollTo(0, 0);
 
   return (
-    <section className='filter-container'>
-      <h5>this is the filter box</h5>
-    </section>
+    <form className='filter-container'>
+    <p>Filter mentors by:</p>
+      <select
+        className='select-box'
+        onChange={(e) => setFieldFilter(e.target.value)}
+      >
+        <option value=''>Career Field</option>
+        <option value='Agriculture'>Agriculture</option>
+        <option value='Biology'>Biology</option>
+        <option value='Botany'>Botany</option>
+        <option value='Business/finance'>Business/Finance</option>
+        <option value='Business/marketing'>Business/Marketing</option>
+        <option value='Chemistry'>Chemistry</option>
+        <option value='Construction'>Construction</option>
+        <option value='Education'>Education</option>
+        <option value='Fine arts'>Fine Arts</option>
+        <option value='Law/legal'>Law/Legal</option>
+        <option value='Law enforcement'>Law Enforcement</option>
+        <option value='Medicine'>Medicine</option>
+        <option value='Restaurateur/Chef'>Restaurateur/Chef</option>
+        <option value='Software Development'>Software Development</option>
+        <option value='Other'>Other</option>
+      </select>
+    </form>
   );
 }
 
